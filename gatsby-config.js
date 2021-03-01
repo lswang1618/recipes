b0516711b9e92b6cefcf908ffc4c710f10a87461
lswang1618@gmail.com <lswang1618@gmail.com>
2021-02-28 21:54:03 -0500

    contentful set up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,28 @@
 module.exports = {
+    siteMetadata: {
+      title: `Recipes`,
+      description: `Delicious, easy, and simple Chinese cooking.`,
+      author: ``,
+      siteUrl: `http://www.almostfun.org`
+    },
     plugins: [
-
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+              name: `images`,
+              path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`, 
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+              spaceId: `yu3ibkoph9hr`,
+              // Learn about environment variables: https://gatsby.dev/env-vars
+              accessToken: "pg9L-Gwa9oGeYLIbRfjo9egc2m5a7IrkS2uwAiIVq8o",
+              downloadLocal: true,
+            },
+        },
     ]
 }
