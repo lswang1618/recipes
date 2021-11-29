45866a74896fecdb0ba273c85967e27f177d6d50
Lisa Wang <lisa@almostfun.org>
2021-11-28 20:01:28 -0500

    Revert "update favicon"
    This reverts commit efbed44195d307c75654ed19eeedc90849da5fdd.

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,11 +30,5 @@ module.exports = {
               downloadLocal: true,
             },
         },
-        {
-          resolve: `gatsby-plugin-manifest`,
-          options: {
-            icon: `src/images/logo2.png`, // This path is relative to the root of the site.
-          },
-        },
     ]
 }
