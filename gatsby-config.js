141e5b3e66e249b9f9f4435e5fc260076ef17675
Lisa Wang <lisa@almostfun.org>
2022-05-08 13:45:26 -0400

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,10 +37,23 @@ module.exports = {
           },
         },
         {
-          resolve: `gatsby-plugin-google-analytics`,
+          resolve: `gatsby-plugin-google-gtag`,
           options: {
-            trackingId: "G-TE37X01FGF",
-            head: true,
+            // You can add multiple tracking ids and a pageview event will be fired for all of them.
+            trackingIds: [
+              "G-TE37X01FGF", // Google Analytics / GA
+            ],
+            // This object is used for configuration specific to this plugin
+            pluginConfig: {
+              // Puts tracking script in the head instead of the body
+              head: true,
+              // Setting this parameter is also optional
+              respectDNT: true,
+              // Avoids sending pageview hits from custom paths
+              exclude: ["/preview/**", "/do-not-track/me/too/"],
+              // Defaults to https://www.googletagmanager.com
+              origin: "https://www.88chinesedishes.com",
+            },
           },
         },
     ]
